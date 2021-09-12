ff1475a93514a2f22ea6f03162556fd844eb22e8
pritvish <pritvish.mandal80@gmail.com>
2021-09-13 01:22:43 +0530

    initial commit

---
+++ src/main/java/com/airline/ancillaryflightservices/AncillaryFlightServicesApplication.java
@@ -0,0 +1,13 @@
+package com.airline.ancillaryflightservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AncillaryFlightServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AncillaryFlightServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airline/ancillaryflightservices/AncillaryFlightServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline.ancillaryflightservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AncillaryFlightServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
